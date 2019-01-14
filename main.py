07513b27ba26084a6453565cdb958090f2ee1ca1
AfonsoGR <44871197+AfonsoGR@users.noreply.github.com>
2019-01-14 17:39:00 +0000

    Add files via upload

--- main.py
+++ main.py
@@ -1,43 +1,5 @@
 from math import *
 
-'''
-def soma_matriz(m1, m2):
-	col = len(m1[0])
-	lin = len(m1)
-	if len(m1) != len(m2):
-		return 0
-	else:
-		Madic = [[0 for c in range(col)]for l in range(lin)]
-		for i in range(lin):
-			for j in range(col):
-				Madic[i][j] = m1[i][j] + m2[i][j]
-		return Madic #soma de matrizes
-
-#Produto entre matrizes, recebe 2 matrizes (nao interessa o tamanho destas mas invalido se nao for possivel)
-def prod_matriz(m1, m2):
-	col = len(m1[0]) #numero de colunas
-	lin = len(m2) #numero de linhas
-	Mprod = [[0 for c in range(col)]for l in range(lin)] #inicializaacao dos espacos da matriz produto com numero de colunas do m1 e linhas do m2
-	a = 0 #guarda a soma
-
-	for i in range(lin):
-		for j in range(col):
-			a = 0
-			for k in range (len(m1[0])):
-				a += m1[i][k]*m2[k][j]
-			Mprod[i][j] = a
-	return Mprod #matriz produto
-
-def ler_matriz():
-	M =[] #matriz final, inicializacao
-	dim = int(input("Insira a dimensao da matriz: ")) #dimensao da matriz
-	print("Insira a matriz por cada linha: ")
-	for i in range(dim):
-		v = [int(x) for x in input("Insira a Linha nr "+ str(i+1) +": ").split()]
-		M.append(v)
-	return M
-'''
-
 def matriz_Translacao(vetor_translacao): #cria a matriz translacao para ser usada na funcao de produto de funcoes
         matriz = []
         matriz[0] = [1,0,0,vetor_translacao[0]]
@@ -49,24 +11,38 @@ def matriz_Translacao(vetor_translacao): #cria a matriz translacao para ser usad
 
 def matriz_reflexao(plano): #cria a matriz adequada a reflexao para ser usada na funcao de produto de funcoes
         matriz = []
-        if plano == "XZ":
-                matriz = [[1,0,0,0][0,-1,0,0][0,0,1,0][0,0,0,1]]
-        elif plano == "XY":
-                matriz = [[1,0,0,0][0,1,0,0][0,0,-1,0][0,0,0,1]]
-        elif plano == "YZ":
-                matriz = [[-1,0,0,0][0,1,0,0][0,0,1,0][0,0,0,1]]
+        print("""
+Planos:
+XZ.........[1]
+XY.........[2]
+YZ.........[3]""")
+        while True:
+                plano = int(input("""\nSobre qual plano pretende refletir?\n"""))
+                if plano == "1":#plano XZ
+                        matriz = [[1,0,0,0][0,-1,0,0][0,0,1,0][0,0,0,1]]
+                elif plano == "2":#plano XY
+                        matriz = [[1,0,0,0][0,1,0,0][0,0,-1,0][0,0,0,1]]
+                elif plano == "3":#plano YZ
+                        matriz = [[-1,0,0,0][0,1,0,0][0,0,1,0][0,0,0,1]]
+                else:
+                        print("Por favor escolha um plano válido.\n")
         return matriz
 
 
 def matriz_rotacao(angulo, eixo): #cria matriz adequada a rotacao com o angulo e eixo que se quer fazer a mesma
         a = math.radians(angulo) #angulo precisa de estar em radianos para ser passada para a funcao cos() e sen()
         matriz = []
-        if eixo == "X":
-                matriz = [[1,0,0,0][0,cos(a),-(sen(a)),0][0,sen(a),cos(a),0][0,0,0,1]]
-        elif eixo == "Y":
-                matriz = [[cos(a),0,sen(a),0][0,1,0,0][-(sen(a)),0,cos(a),0][0,0,0,1]]
-        elif eixo == "Z":
-                matriz = [[cos(a),-(sen(a)),0,0][sen(a),cos(a),0,0][0,0,1,0][0,0,0,1]]
+        eixo = int(input("\nQuer fazer a rotação em que eixo?\n"))
+        angulo = int(input("Qual o ângulo da rotação?\n")) 
+        while True:
+                if eixo == "X":
+                        matriz = [[1,0,0,0][0,cos(a),-(sen(a)),0][0,sen(a),cos(a),0][0,0,0,1]]
+                elif eixo == "Y":
+                        matriz = [[cos(a),0,sen(a),0][0,1,0,0][-(sen(a)),0,cos(a),0][0,0,0,1]]
+                elif eixo == "Z":
+                        matriz = [[cos(a),-(sen(a)),0,0][sen(a),cos(a),0,0][0,0,1,0][0,0,0,1]]
+                else:
+                        print("Por favor escolha um eixo válido.\n")
         return matriz
 
 
@@ -78,6 +54,8 @@ def matriz_tesoura(desvioA,desvioB, eixo): #cria matriz adequada a tesoura com o
                 matriz = [[1,0,0,0][desvioA,1,desvioB,0][0,0,1,0][0,0,0,1]]  
         elif eixo == "Z":
                 matriz = [[1,0,0,0][0,1,0,0][desvioA,desvioB,1,0][0,0,0,1]]
+        else:
+                print("Por favor escolha um eixo válido.\n")
         return matriz
 
 
@@ -92,7 +70,6 @@ def prod_matriz(matriz_operacao, ponto): #Produto entre matrizes, recebe 2 matri
 	lin = len(ponto) #numero de linhas
 	ponto_apos_operacao = [[0 for c in range(col)]for l in range(lin)] #inicializaacao dos espacos da matriz produto com numero de colunas do m1 e linhas do m2
 	a = 0 #guarda a soma
-
 	for i in range(lin):
 		for j in range(col):
 			a = 0
@@ -100,6 +77,7 @@ def prod_matriz(matriz_operacao, ponto): #Produto entre matrizes, recebe 2 matri
 				a += matriz_operacao[i][k]*ponto[k][j]
 			ponto_apos_operacao[i][j] = a
 	return ponto_apos_operacao #matriz produto
+
 
 #quase inutil
 def translação(matriz): #guess what this is
@@ -148,23 +126,6 @@ YZ.........[3]""")
         return m
                 
 
-
-
-'''
-TRANSLAÇÃO
-
-[[1,0,0,tx],[0,1,0,ty],[0,0,1,tz],[0,0,0,1]] * [[x],[y],[z],[1]]
-
-1 * x  +  0 * y  +  0 * z  +  tx * 1  =  x+tx
-x´= x+tx
-
-XZ, XY, YZ
-
-'''
-
-
-
-
 listaPontos = [] #lista para guardar os pontos todos
 
 numeroPontos = int(input("Quantos pontos quer inserir? ")) # pedir o numero de pontos que o utilizador quer
@@ -178,10 +139,6 @@ for i in range(numeroPontos): #loop para dar input dos pontos
 
 print(listaPontos)
 #listaPontos = MATRIZ nao, isto nao e verdade, a lista de pontos e unicamente para guardar os pontos inseridos pelo utilizador, nao pode ser usada para contas diretamente, apenas para recolher os pontos.
-
-
-
-
 
 
 #lista para selecionar a transformacao
@@ -202,10 +159,14 @@ while True:
                 print("5")
                 break
         elif opcao == 4:
-                reflexão(listaPontos)
+                reflexão(listaPontos)#funciona na reflexão errada
                 break
         elif opcao == 3:
-                print("3")
+                print("3") #NÃO FUNCIONA
+                fator_de_escala = [int(x) for x in input ("Insire o factor de escala: ")]
+                matriz_E = matriz_escala(fator_de_escala)
+                for ponto in listaPontos:
+                        prod_matriz(matriz_E, ponto)
                 break
         elif opcao == 2:
                 print("2")
@@ -220,32 +181,3 @@ while True:
                 print("Numero invalido.\n")
 
 print(listaPontos)
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-		
